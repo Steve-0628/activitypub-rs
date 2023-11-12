551490c3502e0b1c17acf3fcdb66e614f8a59468
Steve-0628 <github@hmpf.club>
2023-11-12 23:39:37 +0900

    fix indent

--- src/utils/signature.rs
+++ src/utils/signature.rs
@@ -2,7 +2,7 @@ use reqwest::Request;
 
 
 pub(crate) async fn sign_to_reqest(req:&mut Request) -> &mut Request {
-	let mut headers = req.headers_mut();
+    let mut headers = req.headers_mut();
 
-	req
+    req
 }
